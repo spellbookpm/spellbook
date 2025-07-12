2eba98c7a0b94510d189c32da3aa416d68daa396
Todd Martin <kj4ntv@gmail.com>
2025-07-12 15:51:11 -0700

    feat: add gitops for fetching and pulling a repo

--- lib/spellbook/gitops.ex
+++ lib/spellbook/gitops.ex
@@ -5,11 +5,33 @@ defmodule Spellbook.GitOps do
     dbg(git_path)
 
     case System.cmd(git_path, ["clone", url], cd: target) do
-      {"", 0} -> :ok
-      {message, errorCode} -> 
-        IO.puts("#{errorCode}: #{message}")
+      {_message, 0} -> :ok
+      {message, error_code} -> 
+        IO.puts("#{error_code}: #{message}")
         :error
-      {[messages], errorCode} ->
+      {[messages], error_code} ->
+        :error
+    end
+  end
+
+  def fetch_and_pull(cwd) do
+    {ok, git_path} = Spellbook.Utils.get_executable_path("git")
+
+    case System.cmd(git_path, ["fetch", "--all"], cd: cwd) do
+      {_message, 0} -> :ok
+      {message, error_code} ->
+        IO.puts("#{error_code}: #{message}")
+        :error
+      {[messages], error_code} ->
+        :error
+    end
+
+    case System.cmd(git_path, ["pull"], cd: cwd) do
+      {_message, 0} -> :ok
+      {message, error_code} ->
+        IO.puts("#{error_code}: #{message}")
+        :error
+      {[messages], error_code} ->
         :error
     end
   end
