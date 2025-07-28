2516680e03cdb57ede41be7a99ac791283f8297e
Todd Martin <kj4ntv@gmail.com>
2025-07-27 21:08:15 -0700

    chore: some clean up

--- lib/spellbook.ex
+++ lib/spellbook.ex
@@ -3,19 +3,6 @@ defmodule Spellbook do
   Documentation for `Spellbook`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Spellbook.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def main(args) do
     Spellbook.CLI.main(args)
   end
--- test/spellbook_test.exs
+++ test/spellbook_test.exs
@@ -2,7 +2,4 @@ defmodule SpellbookTest do
   use ExUnit.Case
   doctest Spellbook
 
-  test "greets the world" do
-    assert Spellbook.hello() == :world
-  end
 end
